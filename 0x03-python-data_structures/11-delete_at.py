27979c629dc723b069cbe8896045c794a0cce7cf
Helmeppo <gamerblliissard@gmail.com>
2023-01-12 00:55:17 -0800

    Averted?

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,3 +4,8 @@ def delete_at(my_list=[], idx=0):
     if idx >= 0 and idx < len(my_list):
         del my_list[idx]
     return my_list
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
