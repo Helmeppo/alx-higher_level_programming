28f397ef16b6e5d2e1a740e6ed0ace81c59090c8
Helmeppo <113808268+Helmeppo@users.noreply.github.com>
2023-01-13 02:48:41 +0000

    Create 8-simple_delete.py

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+
+    return a_dictionary
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Statu
