f63a96565ee97951ea4c7aef335aae7272f43ade
Helmeppo <113808268+Helmeppo@users.noreply.github.com>
2023-01-13 02:39:22 +0000

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
-def square_matrix_simple(matrix=[]):
+def square_matrix_s
+    def square_cells(array):
+        result = []
+        for cell in array:
+            result.append(cell ** 2)
+        return resultimple(matrix=[]):
+
     return [list(map(lambda x: x ** 2, row)) for row in matrix]
