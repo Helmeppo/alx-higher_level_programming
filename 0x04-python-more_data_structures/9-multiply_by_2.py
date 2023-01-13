8d8e1f849da9961eaf933fe02ec553c70695cff1
Helmeppo <113808268+Helmeppo@users.noreply.github.com>
2023-01-13 02:49:39 +0000

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     """returns a new dictionary with"""
     """all values multiplied by 2"""
     new_dict = {x: (a_dictionary[x] * 2) for x in a_dictionary}
-    return 
+    return new_dict
