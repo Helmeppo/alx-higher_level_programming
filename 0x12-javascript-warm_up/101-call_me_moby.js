cd46ddf82df899d45bfef183ebb2eda206a0c1cd
Helmeppo <113808268+Helmeppo@users.noreply.github.com>
2023-04-12 15:34:22 +0000

    Create 101-call_me_moby.js

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
